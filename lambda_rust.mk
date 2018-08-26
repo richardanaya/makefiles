5dd5aa76d1f4e28fc452cc9c63cc9a5e8b5c11ff
Richard Anaya <richard.anaya@gmail.com>
2018-08-26 06:48:50 +0000

    adding toml too

--- lambda_rust.mk
+++ lambda_rust.mk
@@ -7,13 +7,14 @@ LAMBDA_RUST__LAMBDAS      = $(addprefix $(LAMBDA_RUST__OUTPUT_DIR), $(LAMBDA_RUS
 LAMBDA_RUST__RUST_ROOTS   = $(addprefix $(LAMBDA_RUST__LAMBDAS_BASE), $(LAMBDA_RUST__LAMBDAS_DIRS))/src
 LAMBDA_RUST__RUST_FILES   = $(shell find $(LAMBDA_RUST__RUST_ROOTS) -type f -regex ".*\.rs")
 LAMBDA_RUST__RUST_LOCKS   = $(addprefix $(LAMBDA_RUST__LAMBDAS_BASE), $(LAMBDA_RUST__LAMBDAS_DIRS))/Cargo.lock
+LAMBDA_RUST__RUST_TOML   = $(addprefix $(LAMBDA_RUST__LAMBDAS_BASE), $(LAMBDA_RUST__LAMBDAS_DIRS))/Cargo.toml
 
 lambda_rust__build_pre:
 	@$(call log, "lambda_rust", "Building")
 
 lambda_rust__build: lambda_rust__build_pre $(LAMBDA_RUST__LAMBDAS)
 	
-$(LAMBDA_RUST__OUTPUT_DIR)%: $(LAMBDA_RUST__RUST_FILES) $(LAMBDA_RUST__RUST_LOCKS)
+$(LAMBDA_RUST__OUTPUT_DIR)%: $(LAMBDA_RUST__RUST_FILES) $(LAMBDA_RUST__RUST_LOCKS) $(LAMBDA_RUST__RUST_TOML)
 	echo $(LAMBDA_RUST__RUST_FILES)
 	@cd $(LAMBDA_RUST__LAMBDAS_BASE)$* && cargo build --release
 	@mkdir -p $(LAMBDA_RUST__OUTPUT_DIR)
